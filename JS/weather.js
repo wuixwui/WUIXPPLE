10dc8091e7ebda1fea6d900b405594b82e03b83d
wuixwui <seoweet21@gmail.com>
2021-08-16 22:43:54 +0900

    feat: 날씨 정보 추가
    Use current API

--- JS/weather.js
+++ JS/weather.js
@@ -1,1 +1,17 @@
-function onGeoOk(position) {}
+const API_key = "103b044b3af7e9f56ec2cce893a7f5fb";
+const weather = document.querySelector(".today__weather");
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      weather.innerText = `(${data.name}) ${data.weather[0].main}`;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
